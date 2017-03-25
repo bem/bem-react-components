f3f67793f78dca7c69eafdd15ba48c3ddd57360d
Sergey Berezhnoy <veged@yandex-team.ru>
2017-03-25 22:53:29 +0300

    wip

--- .bemrc.js
+++ .bemrc.js
@@ -5,20 +5,33 @@ module.exports = {
             scheme : 'nested',
             schemeOptions : 'react',
             naming : 'react'
+        },
+        'blocks/**/*.tests' : {
+            scheme : 'flat',
+            schemeOptions : 'react',
+            naming : 'react'
         }
     },
     modules : {
         'bem-tools' : {
             plugins : {
                 create : {
-                    levels : {
-                        blocks : {
-                            default : true
-                        }
-                    },
-                    techs : ['js', 'css'],
+                    techs : ['js', 'spec.js', 'css', 'tests' ],
                     templates : {
-                        js : '.bem/templates/js.js'
+                        js : '.bem/templates/js.js',
+                        'spec.js' : '.bem/templates/spec.js.js',
+                        'tests' : '.bem/templates/tests.js',
+                    },
+                    levels : {
+                        blocks : { default : true },
+                        'blocks*/**/*.tests' : {
+                            techs : ['js', 'css', 'html'],
+                            templates : {
+                                js : '.bem/templates/tests-js.js',
+                                css : '.bem/templates/tests-css.js',
+                                html : '.bem/templates/tests-html.js',
+                            }
+                        }
                     }
                 }
             }

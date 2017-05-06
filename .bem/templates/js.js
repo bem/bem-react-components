bb3840095cf787df73378a9545b6b62954c33ff1
Anton Winogradov <winogradovaa@gmail.com>
2017-05-07 02:36:06 +0300

    .bemrc (#33)

--- .bem/templates/js.js
+++ .bem/templates/js.js
@@ -1,30 +1,22 @@
-var EOL = require('os').EOL,
-    nameRegEx = /^[a-zA-Z_][a-zA-Z\d_]*$/;
-
+const nameRegEx = /^[a-zA-Z_][a-zA-Z\d_]*$/;
 
 function toObjectKey(str) {
-    return nameRegEx.test(str) ? str : "'" + str + "'"
+    return nameRegEx.test(str) ? str : `'${str}'`;
 }
 
 function toObjectValue(x) {
-    return typeof x === 'boolean' ? x : "'" + x + "'";
+    return typeof x === 'boolean' ? x : `'${x}'`;
 }
 
-module.exports = function (entity, naming) {
-    return [
-        "import {decl} from 'bem-react-core';",
-        "",
-        "export default " +
-        (entity.modName ?
-            "declMod({ " +
-                toObjectKey(entity.modName) + " : " +
-                toObjectValue(entity.modVal || true) +
-            " }, {" :
-            "decl({"),
-        "    block : '" + entity.block + "'" +
-        (entity.elem ?
-            "," + EOL + "    elem : '" + entity.elem + "'" :
-            ""),
-        "});"
-    ].join(EOL);
+module.exports = function ({ block, elem, mod={} }, naming) {
+    const { name : modName, val : modVal } = mod;
+    return `import { ${modName? 'declMod' : 'decl'} } from 'bem-react-core';
+
+export default ${modName?
+            `declMod({ ${toObjectKey(modName)} : ${toObjectValue(modVal || true)} }, {` :
+            'decl({'}
+    block : '${block}'${elem? `,
+    elem : '${elem}'` : ''}
+});
+`;
 };
---
+++ .bemrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    root : true,
+    levels : {
+        blocks : {
+            scheme : 'nested',
+            shemeOptions : 'react',
+            naming : 'react'
+        }
+    },
+    modules : {
+        'bem-tools' : {
+            plugins : {
+                create : {
+                    levels : {
+                        blocks : {
+                            default : true
+                        }
+                    },
+                    techs : ['js', 'css'],
+                    templates : {
+                        js : '.bem/templates/js.js'
+                    }
+                }
+            }
+        }
+    }
+};
